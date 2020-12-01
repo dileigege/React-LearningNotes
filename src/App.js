bf8ef62cbb87a9669a27e697fdeecbf26c87190f
dileigege <1637393255@qq.com>
2020-12-01 13:42:28 +0800

    React循环 && 判断

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @@Company: DCIT-SH
  * @Author: Oneself
  * @Date: 2020-11-30 17:53:03
  * @LastEditors: Oneself
- * @LastEditTime: 2020-12-01 11:18:44
- * @Statu: TODO: 
+ * @LastEditTime: 2020-12-01 13:41:09
+ * @Statu: TODO:
  */
-import React from 'react';
-import ReactDOM from 'react-dom'
-import HeaderComponents from './components/header'
-// 
-class App extends  React.Component{
-  componentDidMount(){
-    // console.log(document.getElementById('forData'))
-    console.log(this.refs['forData'])
-    console.log(ReactDOM.findDOMNode(document.getElementById('herder')))
+import React from "react";
+import ReactDOM from "react-dom";
+import HeaderComponents from "./components/header";
+import "./assets/css/app.css";
+
+//
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: false,
+      data: [
+        { id: 1, title: "html" },
+        { id: 2, title: "css" },
+        { id: 3, title: "JS" },
+      ],
+    };
+
+    this.ClickIsShow = this.ClickIsShow.bind(this);
   }
-  render(){
-    let name = '你好呀';
-    let conter ="<span style='color:#FF0000'> 我是html </span>";
-    
-    return (  
+  componentDidMount() {}
+  ClickIsShow() {
+    this.setState({ isShow: !this.state.isShow });
+  }
+  render() {
+    return (
       <div className="App">
-        {/* JSX语法 */}
-        {name}
-        {/* JSX */}
-        <div dangerouslySetInnerHTML={{__html:conter}}></div>
-         {/* ref JSX */}
-        <div ref="forData"> 获取数据 </div>
         <HeaderComponents></HeaderComponents>
+        {this.state.isShow ? <div className="judge"></div> : ""}
+        {this.state.isShow && <div className="judge"></div>}
+        <button type="button" onClick={this.ClickIsShow}>显示 \ 隐藏</button>
+        <div>
+          {this.state.data.map((item, index) => {
+            return (
+              <p key={index}>
+                <span>{item.title}</span>
+              </p>
+            );
+          })}
+        </div>
       </div>
     );
   }
